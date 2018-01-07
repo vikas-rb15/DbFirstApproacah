9043355fa490d8d3ec1efc78d2ef91d134ebca1c
vikas-rb15 <34038926+vikas-rb15@users.noreply.github.com>
2018-01-07 18:57:32 +0530

    added new mvc application project using entity frame work core

---
+++ DemoDbFirstAproach/Controllers/ActorController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DemoDbFirstAproach.Models;
+
+namespace DemoDbFirstAproach.Controllers
+{
+    public class ActorController : Controller
+    {
+        private ActorDbContext _dbContext;
+
+        public ActorController(ActorDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IActionResult Index()
+        {
+            return View(_dbContext.Actors.ToList());
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Actors actor)
+        {
+            if (ModelState.IsValid)
+            {
+                _dbContext.Add(actor);
+                _dbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(actor);
+        }
+
+    }
+}
---
+++ DemoDbFirstAproach/Models/ActorDbContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using DemoDbFirstAproach.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace DemoDbFirstAproach.Models
+{
+    public partial class ActorDbContext : DbContext
+    {
+        public virtual DbSet<Actors> Actors { get; set; }
+
+        public ActorDbContext(DbContextOptions<ActorDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+
+        public static implicit operator ActorDbContext(ActorController v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
